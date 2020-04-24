a9aed9384efaa8bd2cbce0d461b4b2c3fca44de8
Thanujakallamadi <kallamadithanuja@gmail.com>
2020-04-24 10:54:46 -0700

    'message'

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import './index.css';
+
+//const history=createBrowserHistory();
+
+ReactDOM.render(
+    
+    <App/>
+    ,document.getElementById('root')
+    
+    );
+
